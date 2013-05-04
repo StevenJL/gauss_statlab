f94025eecddfc54e9024f6957e83c1ff3d21b12c
Steven J. Li <StevenJLi@gmail.com>
2013-05-03 21:01:09 -0700

    just installed statsample gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaussLab::Application.config.secret_token = 'a20ca1799d624d2f1a8b159eb0bd7cf8b22e76cf51d7ab3fd7e65c0d771a81a301f242d8630e6f80dc951aa2b53877911ddb6c2e2a0a1602bc18d1bcb7380f24'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+GaussLab::Application.routes.draw do
+
+  root :to => "landing_page#index"
+
+
+end
