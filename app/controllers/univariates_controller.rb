623ceb0a5e861c54231fb53aa4ec66f6aacf957a
Steven J. Li <StevenJLi@gmail.com>
2013-05-04 15:42:24 -0700

    adding dom elements to univariates

--- app/controllers/univariates_controller.rb
+++ app/controllers/univariates_controller.rb
@@ -6,6 +6,7 @@ class UnivariatesController < ApplicationController
   def compute
     input_data = params[:input_data]
     results = Univariate.new(input_data).get_basic_stats
+    p results
     render :json => results
   end
 
--- app/models/univariate.rb
+++ app/models/univariate.rb
@@ -7,16 +7,22 @@ class Univariate
   def get_basic_stats
     output = {}
     input_data_array = @input_data.split(",")
+    input_data_array = input_data_array.map {|int| int.to_f }
     vector = input_data_array.to_scale
     output[:n] = vector.size
+    output[:min] = vector.min
+    output[:first_quartile] = vector.percentil(25)
     output[:mean] = vector.mean
     output[:median] = vector.median
+    output[:third_quartile] = vector.percentil(75)
+    output[:max] =  vector.max
     output[:s] = vector.sd
     output[:sigma] = vector.standard_deviation_population
     output[:variance_sam] = vector.variance
     output[:variance_pop] = vector.variance_population
     output[:ss] = vector.ss
     output[:kurtosis] = vector.kurtosis
+    output
   end
 
 end
