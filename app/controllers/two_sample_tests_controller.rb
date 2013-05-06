04035135eaf3cee9be778803a772326111bbf1d2
Steven J. Li <StevenJLi@gmail.com>
2013-05-06 11:44:52 -0700

    finished two sample t test

--- app/controllers/two_sample_tests_controller.rb
+++ app/controllers/two_sample_tests_controller.rb
@@ -3,17 +3,15 @@ class TwoSampleTestsController < ApplicationController
   def show
   end
 
-
   def compute    
     p params
     sample1 = params[:sample1]
     sample2 = params[:sample2]
     test_type = params[:test_type]
     equal_var = params[:equal_var]
-    results = TwoSampleTest.new(sample1, sample2, test_type, equal_var)
+    results = TwoSampleTest.new(sample1, sample2, test_type, equal_var).get_results
     render :json => results
   end
 
-
-
 end
+
--- app/models/two_sample_test.rb
+++ app/models/two_sample_test.rb
@@ -1,17 +1,15 @@
 class TwoSampleTest
 
 
-  def initialize(sample1, sample2, test_type, equal_var)
-    @sample1 = sample1
-    @sample2 = sample2
+  def initialize(input_sample1, input_sample2, test_type, equal_var)
+    input_sample1 = input_sample1.split(",")
+    input_sample1 = input_sample1.map {|int| int.to_f }
+    input_sample2 = input_sample2.split(",")
+    input_sample2 = input_sample2.map {|int| int.to_f }
+    @sample1 = input_sample1.to_scale
+    @sample2 = input_sample2.to_scale
     @test_type = test_type
     @equal_var = equal_var
-  end
-
-  def get_results
-
-
-
   end
 
   def convert_to_tails(test_type)
@@ -24,8 +22,21 @@ class TwoSampleTest
     end
   end
 
-
-
+  def get_results
+    output = {}
+    tst = Statsample::Test::T::TwoSamplesIndependent.new(@sample1,@sample2,{:tails=>convert_to_tails(@test_type)})
+    if @equal_var == "Yes"
+      output[:df] = tst.df_equal_variance
+      output[:t] = tst.t_equal_variance
+      output[:p] = tst.probability_equal_variance
+    else
+      output[:df] = tst.df_not_equal_variance
+      output[:t] = tst.t_not_equal_variance
+      output[:p] = tst.probability_not_equal_variance    
+    end
+    output[:n1] = @sample1.size
+    output[:n2] = @sample2.size
+    output
+  end
 
 end
-
